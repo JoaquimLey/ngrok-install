5f75ae9ca2647c8204f492d8bced69f7262bf4fa
Joaquim Ley <joaquimley@gmail.com>
2020-03-12 10:23:43 +0000

    sed tests

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 #result=${sed -i "s/<add_your_token_here>/$1/g" ngrok.yml}
 
-sed -i "s/<add_your_token_here>/$1/g" ngrok.yml
-cat ngrok.yml
+echo(${sed -i "s/<add_your_token_here>/$1/g" ngrok.yml}
+echo(${sed -i "s/<<us|eu|ap|au>/$2/g" ngrok.yml}
